a024f81f2163f4968e0c57743424c4392a05a8a0
crystalcortez <crystal.cortez@pcc.edu>
2019-02-06 15:05:40 -0800

    commit11

--- moonPhase.js
+++ moonPhase.js
@@ -1,16 +1,16 @@
-//let img;
+let img;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
-	//img = loadImage('assets/moonphoto.png');
+	img = loadImage('assets/moonphoto.png');
 	background(0);
 }
 
 function draw() {
 	
 
-	noFill();
+	//noFill();
 	//stroke(255);
-	//texture(img);
+	texture(img);
 	sphere(windowWidth/10);
 }
