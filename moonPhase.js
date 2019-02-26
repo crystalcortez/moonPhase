c19e6bbf344f4e211ce8d1c7c683a87d0258ba25
crystalcortez <crystal.cortez@pcc.edu>
2019-02-25 16:03:28 -0800

    last commit

--- moonPhase.js
+++ moonPhase.js
@@ -1,13 +1,9 @@
-let img;
 var x = 1;
 var framesElapsed = 0;
 var secondX = 500;
 var phase = 0;
 var shade = 255;
  
-//function preload(){
-//	img = loadImage('assets/moonsky.png');
-//	}
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
@@ -49,30 +45,3 @@ if (x >= 16){
 }
 	fill(0);
 	ellipse(phase, windowHeight/2, size, size);
-	
-	
-	//fill(color);
-	//ellipse(windowWidth/2, windowHeight/2, size, size);
-
-	
-
-//	phase = map(x, 1, 15, windowWidth *0.5, windowWidth* 0.75);
-//	phase = map(x, 16, 30, window Width*0.75, windowWidth*0.5);
-//	fill(100);
-//	ellipse(phase, windowHeight/2, size, size);
-
-}
-
-//	if phase > 16{
-	//push();
-    //fill(255);
-	//ellipse(250,250 , 100, 100);
-	
-
-	//fill(0);
-  //	ellipse(x, 250, 100, 100);
-//fill(200);
-//	ellipse(secondX, 250, 100, 100);
-//	x = x + 1;
-//	pop();
-//	secondX = secondX - 1;
